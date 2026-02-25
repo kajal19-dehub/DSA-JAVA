a030cdac5ca44a2926809e540c570f19d0df085f
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2026-02-25 22:52:32 +0530

    Added new problem

---
+++ printn.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class printn {
+    public static void print(int n){
+
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n+" ");
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print(n);
+    }
+}
