64706ed83b4179e299f5b6690efdde026b8abea2
kajal19-dehub <143339655+kajal19-dehub@users.noreply.github.com>
2026-03-02 23:32:32 +0530

    added new problem

---
+++ traversal.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class traversal {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
